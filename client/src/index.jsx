26eeacd681944852b996309906b7d7a45dc5e730
Alysa Shin <ashin.saga@gmail.com>
2020-12-30 21:12:59 -0800

    reat router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from './Main.jsx';
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>,
+  document.getElementById('app')
+);
